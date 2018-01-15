57ec8370150e8006c39e49080b1288a472c083e4
Randika Wishwani <randika.wishwani@gmail.com>
2018-01-15 09:59:55 -0500

    initialize

---
+++ hex.h
@@ -0,0 +1,6 @@
+#ifndef HEX_H 
+#define HEX_H
+
+void hexDump ( void *addr, int len);
+
+#endif 
--- hexdump3.c
+++ hexdump3.c
@@ -1,64 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SIZE 255
-
-void hexDump ( void *addr, int len) {
-   	int i;
-	char c;
-	unsigned char buffLine[17];
-	unsigned char *pc = (unsigned char*)addr;
-
-	for (i = 0; i < len; i++) 
-	{
-		if ((i % 16) == 0) 
-		{
-			if (i != 0)
-				printf ("  %s\n", buffLine);
-       			if (pc[i] == 0x00) exit(0);
-				printf ("%08x: ", i);
-        	}
-
-       		printf ("%02x", pc[i]);
-        	if ((i % 2) == 1) 
-            		printf (" "); 
-		if ((pc[i] < 0x20) || (pc[i] > 0x7e))
-		{
-            		buffLine[i % 16] = '.';
-        	}
-		else
-		{
-			buffLine[i % 16] = pc[i];
-		}    
-		buffLine[(i % 16) + 1] = '\0';
-	}
-
-	while ((i % 16) != 0)
-	{
-        	printf ("   ");
-        	i++;
-    	}
-	printf ("  %s\n", buffLine);
-}
-
-
+#include <errno.h>
+# include "hex.h" 
+#define SIZE 10000
 
 int main()
 {
         FILE *ptr_file;
         char buff[SIZE];
+	 int errnum;
 		   
 	ptr_file =fopen("input.txt","rb");
 		
    	if (!ptr_file)
 	{
-            printf("returning 1");
-            return 1;
+          	errnum = errno;
+      		fprintf(stderr, "Value of errno: %d\n", errno);
+      		perror("Error printed by perror");
+      		fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
         }
 
         else
 	{
-
 		fgets(buff,SIZE, ptr_file);
             	hexDump ( &buff, strlen(buff));
 		fclose(ptr_file);
