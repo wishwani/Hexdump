e195bbe62d29b968881334e48b3e6cfe9c0c6d4c
Randika Wishwani <randika.wishwani@gmail.com>
2018-01-31 11:45:42 -0500

    Add files via upload

--- hexdump3.c
+++ hexdump3.c
@@ -2,20 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-# include "hex.h" 
+#include "hex.h" 
 
-
-int main()
+int main(int argc, char *argv[])
 {
         FILE *ptr_file;
-        
-	 int errnum;
-		   
-	ptr_file =fopen("input.txt","rb");
+        int errnum;
+	ptr_file =fopen(argv[1],"rb");
 	
-	
-		
-   	if (!ptr_file)
+	if (!ptr_file)
 	{
           	errnum = errno;
       		fprintf(stderr, " %d\n", errno);
@@ -24,17 +19,14 @@ int main()
 
         else
 	{
-		
-		
 		fseek (ptr_file,0,2);
 		long size=ftell(ptr_file);
 		fseek(ptr_file,0,SEEK_SET);
 		
 		char buff[size];
 		fgets(buff,size, ptr_file);
-            	hexDump ( &buff, strlen(buff));
+            	hexDump (&buff,size); 
 		fclose(ptr_file);
 		return 0;
 	}
 }
-    
